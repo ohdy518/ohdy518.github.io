b8c45020d0edbfe86004279f466928a9e10df40b
ohdy518 <ohdy518@gmail.com>
2024-06-26 22:25:54 +0900

    Initial commit

---
+++ portfolio-svelte/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import "../app.css"
+</script>
+
+<style>
+
+</style>
+
+<main class="bg-neutral-900 text-slate-50 min-h-screen flex items-center justify-center">
+    <slot></slot>
+</main>
