669ba764a3770969326dc83532bb86e04665be6e
ohdy518 <ohdy518@gmail.com>
2024-07-11 18:25:42 +0900

    Created a temporary redirection.

--- portfolio-svelte/src/routes/links/tryeverything/+page.svelte
+++ portfolio-svelte/src/routes/links/tryeverything/+page.svelte
@@ -1,3 +1,6 @@
 <script lang="ts">
-    location.href = "https://drive.google.com/drive/folders/1RV8dNMyPsb41GG2-MUEaC6A_OMh8nus7?usp=sharing"
+    import { browser } from '$app/environment';
+    if (browser) {
+        location.href = "https://drive.google.com/drive/folders/1RV8dNMyPsb41GG2-MUEaC6A_OMh8nus7?usp=sharing"
+    }
 </script>
