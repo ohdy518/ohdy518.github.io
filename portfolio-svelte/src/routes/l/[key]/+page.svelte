c321a2b6b52989f7e30888e0e49403a4fbfc2d84
ohdy518 <ohdy518@gmail.com>
2024-07-15 11:53:56 +0900

    Create /l/ alias for /links/

---
+++ portfolio-svelte/src/routes/l/[key]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { page } from "\$app/stores";
+    import { browser } from "$app/environment";
+    // import { getURLAndRedirect } from "./../param-manager.ts"
+
+    const key: string = $page.params.key
+    if (browser) {
+        window.location.href = "/links/" + key
+    }
+</script>
+
+<span>
+    You are being redirected to the requested webpage.
+</span>
