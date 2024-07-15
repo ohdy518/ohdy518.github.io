2159ff4b617a53d951e8c1dcf36805bb57d06101
ohdy518 <ohdy518@gmail.com>
2024-07-15 11:43:41 +0900

    Delete portfolio-svelte/src/routes/links/tryeverything directory

--- portfolio-svelte/src/routes/links/tryeverything/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-    import { browser } from '$app/environment';
-    if (browser) {
-        location.href = "https://drive.google.com/drive/folders/1RV8dNMyPsb41GG2-MUEaC6A_OMh8nus7?usp=sharing"
-    }
-</script>
