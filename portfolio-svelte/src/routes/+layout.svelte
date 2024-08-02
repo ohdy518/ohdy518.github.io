1e1a2c36ab7a1f22999499e99db6f11326698ad7
ohdy518 <ohdy518@gmail.com>
2024-08-02 20:24:43 +0900

    Update contents and layout for /.

--- portfolio-svelte/src/routes/+layout.svelte
+++ portfolio-svelte/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
     import "../app.css"
 </script>
+<!--<script ></script>-->
 
 <main class="bg-neutral-950 text-slate-50 min-h-screen flex items-center justify-center scroll-smooth">
     <slot></slot>
--- portfolio-svelte/src/routes/index.ts
+++ portfolio-svelte/src/routes/index.ts
@@ -1,55 +1,9 @@
-import { browser } from '$app/environment';
+import twemoji from "twemoji"
 
-// let enabled: boolean = false
+import {browser} from "$app/environment";
 
-if (browser) {
-    window.scrollTo(0, 0)
+export function ParseTwemoji() {
+    if (browser) {
+        twemoji.parse(document.body)
+    }
 }
-
-// export function linkToAboutPage() {
-//     if (browser) {
-//         showHiddenByDefaults()
-//         window.scrollTo(
-//             {
-//                 top: document.body.scrollHeight,
-//                 left: 0,
-//                 behavior: 'smooth'
-//             }
-//         )
-//         enabled = true
-//     }
-// }
-//
-// function showHiddenByDefaults() {
-//     let hiddenByDefaultElements = document.querySelectorAll(".hide-me-by-default")!
-//     hiddenByDefaultElements.forEach((element, _) => {
-//         element.classList.remove("h-0")
-//         element.classList.add("h-screen")
-//     })
-// }
-//
-// function loadAboutPage(){
-//     window.location.href = "/about"
-// }
-//
-// function IOHandler(es: IntersectionObserverEntry[]) {
-//     es.forEach(e => {
-//         if (e.isIntersecting && enabled) {
-//             loadAboutPage()
-//         }
-//     })
-// }
-//
-// if (browser) {
-//     let observer = new IntersectionObserver((e) => {IOHandler(e)})
-//
-//     let detectionTarget = document.getElementById("detection-target");
-//
-//     if (detectionTarget == null) {
-//         console.error("detectionTarget is null")
-//     }
-//     else {
-//         observer.observe(detectionTarget)
-//     }
-//
-// }
