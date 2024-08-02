3bf27d6548a87f013a74f721f03d6b214a5436a2
ohdy518 <ohdy518@gmail.com>
2024-08-02 22:40:34 +0900

    Add /underconstruction Made basic /projects and /experiences page (redirects to /underconstruction).

--- portfolio-svelte/src/routes/projects/+page.svelte
+++ portfolio-svelte/src/routes/projects/+page.svelte
@@ -1,30 +1,24 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter"/>
 
 <script lang="ts">
-    import "./../style.css"
-    import "./local-index.ts"
-    import { initializeScrollObserver, scrollAndLinkTo } from "../scroll-to-redirect.ts";
+    import {browser} from "$app/environment";
 
-    initializeScrollObserver()
+    if (browser) {
+        window.location.href = "/underconstruction"
+    }
 </script>
 
-<div id="container" class="flex flex-col min-h-screen max-w-[75%] min-w-[1440px] bg-neutral-800 items-start justify-between">
-    <div id="full-screen" class="min-w-[1080px] h-full mt-[100px] ml-[140px] flex flex-col items-start self-start fadeInOnLoad hide-me">
-        <h1 id="title" class="text-[72px] font-bold ml-c-medium w-fit inter-title">
-            Projects
-        </h1>
-
-        <span id="REPLACE-FROM-HERE" class="mt-c-small text-[36px] font-normal ml-c-medium w-fit">
-
-        </span>
-
-
+<div id="container" class="bg-neutral-950 w-[56.25vh] h-full min-h-screen px-[5.625vh]">
+    <div id="navbar" class="w-max h-c-medium mb-c-medium">
     </div>
-
-    <div id="about-loader" class="hide-me-by-default w-full h-0 justify-end flex flex-col items-end overflow-hidden">
-        <div id="detection-target" class="pt-c-small overflow-hidden w-1"></div>
+    <div id="content" class="mt-c-large">
+        <div id="intro" class="mb-c-medium">
+            <div class="w-full flex">
+                <h1 class="inter text-5xl mb-c-medium text-nowrap w-fit font-semibold" id="hello-text">Projects</h1>
+            </div>
+            <span class="inter text-lg leading-relaxed">
+<!--                TODO: Add content-->
+            </span>
+        </div>
     </div>
-
-
 </div>
-
---
+++ portfolio-svelte/src/routes/underconstruction/+page.svelte
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter"/>
+
+<div id="container" class="bg-neutral-950 w-[56.25vh] h-full min-h-screen px-[5.625vh]">
+    <div id="navbar" class="w-max h-c-medium mb-c-medium">
+        <!--        TODO: Move to +layout.svelte-->
+    </div>
+    <div id="content" class="mt-c-large">
+        <div id="intro" class="mb-c-medium">
+            <div class="w-full flex">
+                <h1 class="inter text-5xl mb-c-medium text-nowrap w-fit font-semibold" id="hello-text">Whoopsies, </h1>
+            </div>
+            <span class="inter text-lg leading-relaxed">
+                You're connection has been hijacked &mdash; and you're now here. <br />
+                The webpage you want is under construction. <br />
+                <br />
+                Once the webpage you want is finished, you'll no longer be hijacked. <br />
+                <br />
+                Please wait until the webpage is finished or
+                <a class="hover:underline text-sky-400" href="/emailme">give&nbsp;the&nbsp;dev&nbsp;a&nbsp;nudge.&nbsp;</a> <br />
+                <br />
+                <a href="/" class="hover:underline text-sky-400">-> Go home</a>
+            </span>
+        </div>
+    </div>
+</div>
