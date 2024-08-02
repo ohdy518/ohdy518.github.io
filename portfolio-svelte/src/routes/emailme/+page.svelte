14eda07202f050b9fdb329f996fb0ae23455894c
ohdy518 <ohdy518@gmail.com>
2024-08-02 22:16:38 +0900

    HOTFIX: grammar in /emailme

--- portfolio-svelte/src/routes/emailme/+page.svelte
+++ portfolio-svelte/src/routes/emailme/+page.svelte
@@ -29,10 +29,10 @@
                 Seems like you've decided to email me. <br />
                 Good news, I know how to read emails.  <br />
                 <br />
-                If you're looking for <span class="jbm">mailto</span> links for sending quick mails,
+                If you're looking for a <span class="jbm">mailto</span> link for sending a quick email,
                 <a class="hover:underline text-sky-400" href="mailto:ohdy518@gmail.com?subject=%5BPORTFOLIO%5D" target="_blank">click&nbsp;here. </a> <br />
                 <br />
-                Alternatively, if you wish to send email manually,
+                Alternatively, if you wish to send one manually,
                 <button class="hover:underline text-sky-400" on:click={revealPersonalInfo}>click&nbsp;here.&nbsp;<span class="text-xs jbm bg-neutral-700 px-1 py-0.5 rounded">Reveal&nbsp;info</span></button> <br />
                 <br />
             </span>
