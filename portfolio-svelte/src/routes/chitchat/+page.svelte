079e08299c9098ca14e8160d707918d35028164b
ohdy518 <ohdy518@gmail.com>
2024-08-03 13:49:05 +0900

    Add /chitchat.

---
+++ portfolio-svelte/src/routes/chitchat/+page.svelte
@@ -0,0 +1,52 @@
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter"/>
+
+<!--<script lang="ts">-->
+<!--    import {browser} from "$app/environment";-->
+
+<!--    if (browser) {-->
+<!--        window.location.href = "/underconstruction"-->
+<!--    }-->
+<!--</script>-->
+
+<div id="container" class="bg-neutral-950 w-[56.25vh] h-full min-h-screen px-[5.625vh]">
+    <div id="navbar" class="w-max h-c-medium mb-c-medium">
+    </div>
+    <div id="content" class="mt-c-large">
+        <div id="intro" class="mb-c-medium">
+            <div class="w-full flex">
+                <h1 class="inter text-5xl mb-c-medium text-nowrap w-fit font-semibold" id="hello-text">Chitchat</h1>
+            </div>
+            <span class="inter text-lg leading-relaxed">
+                <b>Hey, this is a page just for scribbling down my random thoughts. </b> This page will contain nothing important.
+                You can always
+                <a class="hover:underline text-sky-400" href="/">go back to home</a>
+                and not look at this page. Thanks!
+            </span>
+        </div>
+        <hr />
+        <div id="content-python" class="my-c-medium">
+            <span class="inter text-lg leading-relaxed">
+                Hi, I made this page to write down my thoughts in a better organized way, to practice English,
+                to let the future me know what I was up to decades ago, and stuff. If you'd like to know me more,
+                this might be the good webpage. <br />
+                <br />
+                I'm a developer, perhaps a quasi-developer, who's fascinated by the cultures and the outcomes of the
+                developers and its community. I admire the honesty and enlightenment amongst the community, where
+                being open-source, helping others, and making the internet a better place for everyone is teeming and considered
+                obvious. <br />
+                <br />
+                As the About me page says, I love anything related to computers. Aside from programming,
+                I also have interest in user interface design, hardware technologies, laws and regulations regarding the
+                internet, and much more.<br />
+                <br />
+                I like everything to be organized, unlike my desk setup. I make this website too, to show myself in a more neat
+                and organized way. I have some differences in real life and on the internet, one of them being organizing stuff.
+                I, in real life, almost never clean my desk I leave my desk as-is, until it becomes a major health threat.
+                On the other hand, digital me likes everything to be organized. Everything in folders, organized and cleaned up
+                makes me happy. <br />
+                <br />
+                <i>to be continued...</i>
+            </span>
+        </div>
+    </div>
+</div>
