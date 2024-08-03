130ad8329e9a0c9586d785176523688aa14a5934
ohdy518 <ohdy518@gmail.com>
2024-08-03 17:03:55 +0900

    Add period in /chitchat. New writing in /chitchat.

--- portfolio-svelte/src/routes/chitchat/+page.svelte
+++ portfolio-svelte/src/routes/chitchat/+page.svelte
@@ -28,8 +28,8 @@
             <span class="inter text-lg leading-relaxed">
                 Hi, I made this page to write down my thoughts in a better organized way, to practice English,
                 to let the future me know what I was up to decades ago, and stuff. If you'd like to know me more,
-                this might be the good webpage. <br />
-                <br />
+                this might be a good place. <br />
+                <br />&mdash;&mdash;&mdash;<br />
                 I'm a developer, perhaps a quasi-developer, who's fascinated by the cultures and the outcomes of the
                 developers and its community. I admire the honesty and enlightenment amongst the community, where
                 being open-source, helping others, and making the internet a better place for everyone is teeming and considered
@@ -44,8 +44,23 @@
                 I, in real life, almost never clean my desk. I leave my desk as-is, until it becomes a major health threat.
                 On the other hand, digital me likes everything to be organized. Everything in folders, organized and cleaned up
                 makes me happy. <br />
+                <br />&mdash;&mdash;&mdash;<br />
+                Nowadays, at least at the time this paragraph is being written (August of 2024), artificial intelligence
+                seems to be taking over the whole internet. Every single corporation &mdash; from computer companies to telecommunication
+                services providers &mdash; seems as if they're halting everything they were doing and implementing artificial intelligence for their products.
+                AI certainly is not a bad thing, and AI technology advances by competition of many companies.
+                However, I believe it has entered the phase of being annoying, rather than being cool or useful. <br />
                 <br />
-                <i>to be continued...</i>
+                Everything must hold a valid purpose of existence. In terms of products, its purpose should be focusing
+                on the consumers, not the corporation. For example, the iPad collection from Apple holds the purpose of
+                satisfying the needs of mobile large-touchscreen tablet device, such as in drawing digital arts. <br />
+                <br />
+                Some artificial intelligence products hold no purpose for the customers. For instance, win-predicting
+                AI for sport games have no benefits of having, and might even make the game unexciting. Even if the AI
+                is in the state of the art, purposelessly inserting it into the already good products might make the result
+                unappealing as a whole.
+
+                <br /><br /><i>to be continued...</i>
             </span>
         </div>
     </div>
