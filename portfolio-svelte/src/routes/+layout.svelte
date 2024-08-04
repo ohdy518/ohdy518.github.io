69175b61119678c7c8f2dc98ec96707d7271fb42
ohdy518 <ohdy518@gmail.com>
2024-08-04 18:13:32 +0900

    Add meta tag in +layout.svelte for Google Search Console verification.

--- portfolio-svelte/src/routes/+layout.svelte
+++ portfolio-svelte/src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 </script>
 <!--<script ></script>-->
 
+<svelte:head>
+    <meta name="google-site-verification" content="XPP9fSshOFz4jmOGwl_dfeymeT1vFxiSPonombdrFdU" />
+</svelte:head>
+
 <main class="bg-neutral-950 text-slate-50 min-h-full flex items-center justify-center scroll-smooth">
     <slot></slot>
 </main>
