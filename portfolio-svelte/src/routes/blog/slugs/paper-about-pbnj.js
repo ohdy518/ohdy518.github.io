6a2911974a8da3d34f807d17c94fc06913467aa7
ohdy518 <ohdy518@gmail.com>
2024-08-19 18:23:23 +0900

    shh...

---
+++ portfolio-svelte/src/routes/blog/slugs/paper-about-pbnj.js
@@ -0,0 +1,7 @@
+export const content = [{
+    title: "How to Make a PB&J",
+    description: "This blog contains an essay for the recipe of Peanut Butter and Jelly Sandwich. ",
+    content: "<a href='/blog-materials/pbnj/pdf/e0r0.pdf' class='hover:underline text-sky-400'>-> Essay here. </a><br /> <br />" +
+        "Peanut butter and jelly sandwich, being one of the most popular snacks in the United States, has a varying way " +
+        "of making it. I tried to write an 11-paged instructional essay of making one of those. ",
+}]
