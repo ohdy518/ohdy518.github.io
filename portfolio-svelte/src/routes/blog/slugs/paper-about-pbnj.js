0ad80d0e495c4c27b0d3017e910bc9c1d4c701e6
ohdy518 <ohdy518@gmail.com>
2024-09-12 00:18:22 +0900

    Add some details to PB&J blog.

--- portfolio-svelte/src/routes/blog/slugs/paper-about-pbnj.js
+++ portfolio-svelte/src/routes/blog/slugs/paper-about-pbnj.js
@@ -1,8 +1,11 @@
 export const content = [{
     title: "How to Make a PB&J",
-    description: "This blog contains an essay for the recipe of Peanut Butter and Jelly Sandwich. ",
+    description: "Well, if you ever forget to make a PB&J sandwich...",
     content: "<a href='/blog-materials/pbnj/pdf/e0r1.pdf' class='hover:underline text-sky-400'>-> Essay here. </a><br />" +
         "<a href='https://github.com/ohdy518/PBNJ' class='hover:underline text-sky-400'>-> GitHub here. </a><br /> <br />" +
         "Peanut butter and jelly sandwich, being one of the most popular snacks in the United States, has a varying way " +
-        "of making it. I tried to write an 11-paged instructional essay of making one of those. ",
+        "of making it. I tried to write an 11-paged instructional essay of making one of those. <br /><br />" +
+        "The essay and the code was written as part of my school project. <i>I'm not a person who writes an 11-paged essay for no reason.</i>" +
+        "<br /><br />" +
+        "Report all bugs and logical errors to <a class='jbm text-base text-sky-400 hover:underline' href='mailto:pbnj-report@ohdy.is-a.dev'>pbnj-report@ohdy.is-a.dev</a>. Happy cooking.",
 }]
