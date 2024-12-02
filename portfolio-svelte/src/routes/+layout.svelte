5210e0584fbabd5916d976f7f3c86554ee73fa5d
ohdy518 <ohdy518@gmail.com>
2024-12-02 10:00:30 +0900

    Update +layout.svelte

--- portfolio-svelte/src/routes/+layout.svelte
+++ portfolio-svelte/src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 
 <svelte:head>
     <meta name="google-site-verification" content="XPP9fSshOFz4jmOGwl_dfeymeT1vFxiSPonombdrFdU" />
+    <script defer src="https://cloud.umami.is/script.js" data-website-id="63861b1b-d631-47ea-823e-ac7b33ec3364"></script>
 </svelte:head>
 
 <main class="bg-neutral-950 text-slate-50 min-h-full flex items-center justify-center scroll-smooth">
